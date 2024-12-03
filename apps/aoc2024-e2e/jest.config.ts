3aa23591f398dae67321413c3d9eef6bb74a946b
jafaircl <jafaircl@me.com>
2024-12-02 20:03:46 -0500

    Initial commit

---
+++ apps/aoc2024-e2e/jest.config.ts
@@ -0,0 +1,16 @@
+export default {
+  displayName: 'aoc2024-e2e',
+  preset: '../../jest.preset.js',
+  setupFiles: ['<rootDir>/src/test-setup.ts'],
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/aoc2024-e2e',
+};
